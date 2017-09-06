cc4ab315066f2f6ffc6c212986d6ba61d21b6a26
avacio <lexigee@da-real-MBP.local>
2017-09-05 18:56:11 -0700

    initial commit

---
+++ WeatherGetter.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherGetter.swift
+//  WeatherTask
+//
+//  Created by lexigee on 2017-09-05.
+//
+//
+
+import Foundation
+
+class WeatherGetter {
+
+    private let WeatherAPIKey = "6027c5e0369d7640507be10e2bb14557"
+    private let BaseWeatherURL = "http://api.openweathermap.org/data/2.5/weather"
+
+    func getWeather(city: String) {
+        
+}
